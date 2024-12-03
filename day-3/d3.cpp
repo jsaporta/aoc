3eda17e016eed464ba292e5b23cd8a36a2fc22fb
Jason Saporta <jasons89@gmail.com>
2024-12-03 18:57:31 -0500

    Day 3, part 2 complete

--- day-3/d3.cpp
+++ day-3/d3.cpp
@@ -5,15 +5,22 @@
 #include <vector>
 
 std::vector<int> extractAndMultiply(const std::string& input) {
+	bool enabled = true;
 	std::vector<int> results;
-    	std::regex pattern(R"(mul\((\d+),(\d+)\))");
+	std::regex pattern(R"(mul\((\d+),(\d+)\)|do\(\)|don't\(\))");
     	std::smatch match;
 
     	auto searchStart = input.cbegin();
     	while (std::regex_search(searchStart, input.cend(), match, pattern)) {
-    		int x = std::stoi(match[1].str());
-    	    	int y = std::stoi(match[2].str());
-    	    	results.push_back(x * y);
+		if (match[1].matched && match[2].matched) {
+			int x = std::stoi(match[1].str());
+			int y = std::stoi(match[2].str());
+    	    		results.push_back(enabled ? x * y : 0);
+		} else if (match.str().find("do()") != std::string::npos) {
+			enabled = true;
+		} else if (match.str().find("don't()") != std::string::npos) {
+			enabled = false;
+		}
     	    	searchStart = match.suffix().first; // Move to the rest of the string
     	}
 
